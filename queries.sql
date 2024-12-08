26ba0bad7570d1c6be0829fbe01f040a58930577
fourth36 <zamk36@gmail.com>
2024-12-08 13:30:17 +0800

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -60,3 +60,38 @@ WHERE average_income_calc.average_income < average_income_all.average_income_all
 -- условие средняя выручка продавца меньше средней выручки по всем продавцам--
 ORDER BY average_income;
 
+-- подсчет информации о выручке по дням недели
+
+WITH day_of_week_income as(
+    SELECT
+        first_name || ' ' || last_name AS seller,
+        EXTRACT (ISODOW FROM s.sale_date) AS day_of_week_number, -- день недели цифра
+        to_char(s.sale_date, 'day') AS day_of_week_text, -- день недели текстом
+        SUM(s.quantity*p.price) AS day_of_week_income --суммарная выручка продавца за этот день--
+    FROM sales AS s
+    INNER JOIN products AS p
+        ON s.product_id = p.product_id
+    INNER JOIN employees AS e
+        ON s.sales_person_id = e.employee_id
+    GROUP BY first_name, last_name, s.sale_date
+), -- считает выручку в конкретный день недели --
+
+-- считаем сумарную выручку за конкретный день недели для каждого продавца --
+sum_table as(
+    SELECT
+        seller,
+        day_of_week_number,
+        day_of_week_text,
+        SUM(day_of_week_income) OVER(PARTITION BY seller, day_of_week_number) AS income
+    FROM day_of_week_income
+)
+
+--выводим финальную таблицу --
+SELECT
+    seller,
+    day_of_week_text AS day_of_week,
+    income
+    FROM sum_table
+GROUP BY seller, day_of_week, income, day_of_week_number
+ORDER BY day_of_week_number, seller;
+
